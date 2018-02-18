5d08875066fd959f9a120c1aa9a2a45890015f83
Bartouille <28799444+liqueurdetoile@users.noreply.github.com>
2018-02-18 18:58:47 +0100

    Remoce coveralls support not working

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage-istanbul', 'coveralls'],
+    //reporters: ['progress', 'coverage-istanbul', 'coveralls'],
+    reporters: ['progress', 'coverage-istanbul'],
     coverageIstanbulReporter: {
       reports: ['html', 'lcovonly'],
       dir: './coverage',
